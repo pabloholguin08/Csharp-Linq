928934040169474a4549c4e550539a3f88b2d7c7
Juan Pablo Holguín <jpholguin06@gmail.com>
2023-08-24 19:53:49 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColeccion());
+
+void ImprimirValores(IEnumerable<Book> listadelibros){
+    Console.WriteLine("{0,-60} {1,15} {2,15}\n", "Titulo", "N. Paginas", "Fecha Publicacion");
+    foreach(var item in listadelibros){
+        Console.WriteLine("{0,-60} {1,15} {2,15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
